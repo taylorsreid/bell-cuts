5e78c04f03cca0f1fd8506f8cc0dbd75a36e003d
Taylor Reid <taylorsreid@gmail.com>
2024-02-23 02:27:14 -1000

    basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [totalCash, setTotalCash] = useState<number>(0)
+    const [numberCaptains, setNumberCaptains] = useState<number>(0)
+    const [numberBellman, setNumberBellman] = useState<number>(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const totalPersons:number = numberCaptains + numberBellman
+    const noCaptainCut:number = totalCash / totalPersons
+    const captainCut:number = noCaptainCut * 1.5
+    const totalCaptainCut:number = captainCut * numberCaptains
+    const remainingAfterCaptainCut:number = totalCash - totalCaptainCut
+    const bellmanCut:number = remainingAfterCaptainCut / numberBellman
+
+    return (
+        <>
+            <table>
+                <tr>
+                    <td>
+                        <label htmlFor="totalCash">Total Cash</label>
+                    </td>
+                    <td>
+                        $<input type="number" id="totalCash" step="1" min="0" value={totalCash} onChange={(e) => { setTotalCash(parseInt(e.target.value)) }} />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label htmlFor="numberCaptains">Number of Captains</label>
+                    </td>
+                    <td>
+                        <input type="number" id="numberCaptains" step="1" min="0" value={numberCaptains} onChange={(e) => { setNumberCaptains(parseInt(e.target.value)) }} />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label htmlFor="numberBellman">Number of Bellman</label>
+                    </td>
+                    <td>
+                        <input type="number" id="numberBellman" step="1" min="0" value={numberBellman} onChange={(e) => { setNumberBellman(parseInt(e.target.value)) }} />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label htmlFor="captainCut">Captain's Cut</label>
+                    </td>
+                    <td id='captainCut'>
+                        { captainCut }
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label htmlFor="bellmanCut">Bellman's Cut</label>
+                    </td>
+                    <td id='bellmanCut'>
+                        { bellmanCut }
+                    </td>
+                </tr>
+            </table>
+        </>
+    )
 }
 
 export default App
